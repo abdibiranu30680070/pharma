3f282005ee06b764be9874cd78115fdfb37f47de
Abdi Biranu Tolasa <Abdi.Biranu@gohbetbank.com>
2026-07-27 15:03:51 +0300

    fixed rid

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Handle all other routes by serving index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
